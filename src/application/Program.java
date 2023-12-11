1b4dd8135dedab44f42c1c1f59428ae037ade7c2
Ramon Chaves <144055828+Mushytu@users.noreply.github.com>
2023-12-11 16:05:19 -0300

    Starting to implement Board and Piece

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,17 +2,12 @@ package application;
 
 import java.util.Locale;
 
-import boardgame.Position;
-
 public class Program {
 
 	public static void main(String[] args) {
 		
 		Locale.setDefault(Locale.US);
 		
-		
-		Position pos = new Position(3, 5);
-		System.out.println(pos);
 	}
 
 }
