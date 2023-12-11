2200f40f59239151084ab3d327beec88465cc2c3
Ramon Chaves <144055828+Mushytu@users.noreply.github.com>
2023-12-11 16:40:51 -0300

    Chess layer and printing the board

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,12 +2,16 @@ package application;
 
 import java.util.Locale;
 
+import chess.ChessMatch;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
 		Locale.setDefault(Locale.US);
 		
+		ChessMatch match = new ChessMatch();
+		UI.printBoard(match.getPieces()); 
 	}
 
 }
