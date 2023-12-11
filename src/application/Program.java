915a3d8e0a0de0d255f3a758c79088bbed477956
Ramon Chaves <144055828+Mushytu@users.noreply.github.com>
2023-12-11 15:55:46 -0300

    First class created: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,18 @@
 package application;
 
+import java.util.Locale;
+
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Locale.setDefault(Locale.US);
+		
+		
+		Position pos = new Position(3, 5);
+		System.out.println(pos);
 	}
 
 }
